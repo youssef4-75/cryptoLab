2caea9156aef0d839a03126311966cc0eb90e8e1
haddadi <youssefhaddadi115@gmail.com>
2024-12-27 01:02:09 +0100

    ajouter la methode multilayer, fixer qlq chose dans vigenere et regler la classe Cipher

--- Chiffres/cipher.py
+++ Chiffres/cipher.py
@@ -8,36 +8,28 @@ class Cipher:
         self.__support = main
         self.__in_processing = 0
     
-    def get_support(self):
-        return self.__support
-    
     def __str__(self):
         return self.__support
 
     def __repr__(self):
         return self.__support
     
-    def append(self, letter: str):
-        assert len(letter) == 1
-        self.__support += letter
-    
-    def __shift(self, offset: int):
-        self.__support = (self.__support[:self.__in_processing-1]
-                            + getChar(
-                                getNum(self.__support[self.__in_processing]+offset)
-                            )
-                            + self.__support[self.__in_processing+1:])
-
-    def shift(self, cipher: str|int):
-        
-        r = translate(self.__support[self.__in_processing], cipher)
-
-        self.__support = (self.__support[:self.__in_processing]
-                            + r
-                            + self.__support[self.__in_processing+1:])
-    
     def __len__(self):
         return len(self.__support)
+    
+    def __next__(self):
+        return next(self.__support)
+
+    def __getitem__(self, index):
+        return self.__support[index]
+    
+    def __add__(self, other):
+        assert isinstance(other, Cipher)
+        self.shift_all(other.get_support())
+    
+    def __sub__(self, other):
+        assert isinstance(other, Cipher)
+        self.shift_all(Cipher.complement(other.get_support()))
     
     def lower(self):
         self.__support = self.__support.lower()
@@ -47,23 +39,25 @@ class Cipher:
         self.__support = self.__support.upper()
         return self
 
-    def __getitem__(self, index):
-        return self.__support[index]
+    def get_support(self):
+        return self.__support
+
+    def append(self, letter: str):
+        assert len(letter) == 1
+        self.__support += letter
+
+    # these are the methods that are related to cryptography
+
+    def shift(self, cipher: str|int):
+        
+        r = translate(self.__support[self.__in_processing], cipher)
+
+        self.__support = (self.__support[:self.__in_processing]
+                            + r
+                            + self.__support[self.__in_processing+1:])
 
     def __next(self):
         self.__in_processing += 1
-
-    def __next__(self):
-        return next(self.__support)
-
-    
-    def __add__(self, other):
-        assert isinstance(other, Cipher)
-        self.shift_all(other.get_support())
-    
-    def __sub__(self, other):
-        assert isinstance(other, Cipher)
-        self.shift_all(Cipher.complement(other.get_support()))
 
     def shift_all(self, code:str|int|Iterable[int]|Any):
         if isinstance(code, int):
